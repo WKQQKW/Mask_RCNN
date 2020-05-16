02f7657755eecf33ecea4576e766bb42ae37ed04
Yubao Liu <yubao.liu.ra@tut.jp>
2020-05-16 11:09:03 +0900

    update

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -5,7 +5,10 @@ set -e
 source "/opt/ros/melodic/setup.bash"
 source "/root/catkin_ws/devel/setup.bash"
 
-cd /root/MaskRCNN
+cd /root/catkin_ws/src/MaskRCNN
 python setup.py install
+
+cd /root/catkin_ws
+catkin_make
 
 exec "$@"
